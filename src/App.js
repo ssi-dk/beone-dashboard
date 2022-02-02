88df69df0d564efbedb1713fdca70b99b40552af
Finn Gruwier Larsen <figl@ssi.dk>
2022-02-02 10:06:36 +0100

    Integrate JSX from dashboard1.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function DataView(props){
+  return(
+     <div className="view">
+        <header>
+        <p>{props.title}</p>
+        </header>
+     </div>
   );
 }
 
-export default App;
+function App() {
+  return (
+    <div>
+    <div className="row">
+       <div className="column">
+          <DataView title="Map"/>
+       </div>
+       <div className="column">
+          <DataView title="Tree"/>
+       </div>
+    </div>
+    <div className = "row">
+       <div className="column">
+          <DataView title="Epi"/>
+       </div>
+    </div>
+    <div className = "row">
+       <div className="column">
+          <DataView title="Grid"/>
+       </div>
+    </div>
+ </div>
+  );
+}
