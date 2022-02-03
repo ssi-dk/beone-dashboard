53110344238afbc7a32421593295558252b955b2
Finn Gruwier Larsen <figl@ssi.dk>
2022-02-03 14:46:30 +0100

    Component for uploading a text file.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 // import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 export default App;
+
+class SampleSelector extends Component {
+
+  showFile = async (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = async (e) => { 
+      const text = (e.target.result)
+      console.log(text)
+      alert(text)
+    };
+    reader.readAsText(e.target.files[0])
+  }
+
+  render = () => {
+    return (
+      <div className="pane">
+        <input type="file" onChange={(e) => this.showFile(e)} />
+      </div>
+    )
+  }
+}
 
 function DataView(props){
   return(
@@ -17,9 +40,7 @@ function App() {
     <div className="App">
       <div className="row">
         <div className="column">
-        <div className = "pane">
-            <p>Data folder: {process.env.REACT_APP_DATA_FOLDER}</p>
-        </div>
+          <SampleSelector />
         </div>
         <div className="column">
             <DataView title="Map"/>
