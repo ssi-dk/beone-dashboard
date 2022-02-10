6107b34b09e1a1d611f8b866a9f9a3da3bc72799
Finn Gruwier Larsen <figl@ssi.dk>
2022-02-10 15:34:32 +0100

    Move DataView.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ import {
   useRecoilValue,
 } from 'recoil'
 
-import ReactJson from 'react-json-view'
-
 import './App.css';
 import FileUploader from './FileUploader';
+import DataView from './DataView';
 
 export default App;
 
@@ -35,47 +34,6 @@ function DummyView(props){
         <h1>
         <div>{props.title}</div>
         </h1>
-        {rowItems}
-     </div>
-  );
-}
-
-function DataView(props){
-  const [samples, setSamples] = useRecoilState(sampleState);
-  
-  const rowItems = Object.entries(samples).map(([key, value]) =>
-  <div class="row" key={key}>
-    <div class="column">
-      {key}
-    </div>
-    <div class="column">
-      <ReactJson src={value["sample"]} name="sample" collapsed="true"/>
-    </div>
-    <div class="column">
-      <ReactJson src={value["pipelines"]} name="pipelines" collapsed="true"/>
-    </div>
-  </div>
-  )
-
-  return(
-     <div className="pane">
-      <div class="row row-header">
-        <div class="column">
-          <h1>
-            Data
-          </h1>
-        </div>
-        <div class="column">
-          <h2>
-            Sample
-          </h2>
-        </div>
-        <div class="column">
-          <h2>
-            Pipelines
-          </h2>
-        </div>
-      </div>
         {rowItems}
      </div>
   );
---
+++ src/DataView.js
@@ -0,0 +1,56 @@
+
+import ReactJson from 'react-json-view'
+import {
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+  } from 'recoil'
+export default DataView
+
+const sampleState = atom({
+    key: 'sampleState',
+    default: {},
+  });
+
+function DataView(props){
+    const [samples, setSamples] = useRecoilState(sampleState);
+    
+    const rowItems = Object.entries(samples).map(([key, value]) =>
+    <div class="row" key={key}>
+      <div class="column">
+        {key}
+      </div>
+      <div class="column">
+        <ReactJson src={value["sample"]} name="sample" collapsed="true"/>
+      </div>
+      <div class="column">
+        <ReactJson src={value["pipelines"]} name="pipelines" collapsed="true"/>
+      </div>
+    </div>
+    )
+  
+    return(
+       <div className="pane">
+        <div class="row row-header">
+          <div class="column">
+            <h1>
+              Data
+            </h1>
+          </div>
+          <div class="column">
+            <h2>
+              Sample
+            </h2>
+          </div>
+          <div class="column">
+            <h2>
+              Pipelines
+            </h2>
+          </div>
+        </div>
+          {rowItems}
+       </div>
+    );
+  }
