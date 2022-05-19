121ad0e121c313526ca1ca43075373e5c9ddb23e
Finn Gruwier Larsen <figl@ssi.dk>
2022-05-19 13:51:15 +0200

    Remove console logging.

--- src/DataSources.js
+++ src/DataSources.js
@@ -67,11 +67,11 @@ function DataSources() {
         let sampleId
         if ('sample' in data && 'summary' in data.sample && 'sample' in data.sample.summary) {
           sampleId = data.sample.summary.sample
-          console.log('Assuming BeONE data structure for sample.')
+          // Assuming BeONE data structure for sample.
         }
         if ('name' in data) {
           sampleId = data.name
-          console.log('Assuming Bifrost data structure for sample.')
+          // Assuming Bifrost data structure for sample.
         }
         if (samplesCopy.has(sampleId)) {
           alert(f.name + ' was not imported as it would overwrite existing sample ID ' + sampleId)
