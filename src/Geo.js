8909c29513c18c219c47bc8ff7271ea0a575ae2b
Finn Gruwier Larsen <figl@ssi.dk>
2022-05-25 11:39:08 +0200

    Show a map.

--- src/Geo.js
+++ src/Geo.js
@@ -1,5 +1,6 @@
 import React, { useState, useMemo, useEffect } from 'react'
 import { useRecoilState } from 'recoil'
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 import { sampleState, columnDataState, columnUserdataState } from './RecoilStates'
 
@@ -9,11 +10,19 @@ function Geo() {
   const [samples, setSamples] = useRecoilState(sampleState);
   const [columnData, setColumnData] = useRecoilState(columnDataState);
   const [columnUserdata, setColumnUserdata] = useRecoilState(columnUserdataState);
+  const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
   return (
     <div className='pane'>
-        <h1>Geo</h1>
+      <h1>Geo</h1>
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+          }
+        </Geographies>
+      </ComposableMap>
     </div>
   )
-
 }
