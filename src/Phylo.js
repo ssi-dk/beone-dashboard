9e2897f29cd60cc63687ff90544f85f063808390
Finn Gruwier Larsen <figl@ssi.dk>
2022-10-12 10:54:49 +0200

    Move PhyloClass component to its own file.

--- src/Phylo.js
+++ src/Phylo.js
@@ -3,58 +3,13 @@ import {
   atom,
   useRecoilState,
 } from 'recoil'
-import PhylocanvasGL from '@phylocanvas/phylocanvas.gl'
 import parser from 'biojs-io-newick'
-import PubSub from 'pubsub-js'
 
 import { sampleState, newickState } from './RecoilStates'
 import {readFile, findValues} from './utils'
+import PhyloClass from './PhyloClass'
 
 export default Phylo
-
-class PhyloClass extends React.Component {
-
-  static displayName = 'Phylocanvas'
-
-  canvasRef = React.createRef()
-
-  componentDidMount() {
-    this.tree = new PhylocanvasGL(
-      this.canvasRef.current,
-      { ...this.props } || {},
-    );
-
-    this.tree.handleClick = (info, event) => {
-      const node = this.tree.pickNodeFromLayer(info);
-      if (node && this.props.allIds.includes(node.id)) {
-        this.tree.selectNode(
-          node,
-          event.srcEvent.metaKey || event.srcEvent.ctrlKey,
-        );
-        if (node) {  // or: if (!node.isNull && node.isLeaf)
-          PubSub.publish('SELECT', node.id)
-        }
-      }
-    }
-  }
-
-  componentDidUpdate() {
-    this.tree.setProps({
-      ...this.props,
-    });
-  }
-
-  componentWillUnmount() {
-    this.tree.destroy();
-  }
-
-  render() {
-    return (
-      <div ref={this.canvasRef} />
-    );
-  }
-
-}
 
 function Phylo() {
   const [newick, setNewick] = useRecoilState(newickState);
